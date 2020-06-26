28e81a6d488fbdeb03d4b616fe9efaf14104206a
Bekhan <bigashev25@gmail.com>
2020-06-26 12:46:47 +0600

    sdf

--- index.php
+++ index.php
@@ -1,67 +1,23 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Document</title>
-</head>
-<body>
-<div id="name">
+<?
 
-    <?
+    $queryUrl = 'https://kapmaniyamechty.bitrix24.kz/rest/1/tvhrfsm1tp1gp1q3/user.current.json';
+    // $queryData = http_build_query(array());
 
-     
-        $queryUrl = 'https://'.$_REQUEST['DOMAIN'].'/rest/user.current.json';
-        $params = ['select'=> ['EMAIL', 'NAME', 'LAST_NAME']
-        ];
+    $curl = curl_init();
+    curl_setopt_array($curl, array(
+        CURLOPT_SSL_VERIFYPEER => 0,
+        CURLOPT_POST => 1,
+        CURLOPT_HEADER => 0,
+        CURLOPT_RETURNTRANSFER => 1,
+        CURLOPT_URL => $queryUrl,
+        //CURLOPT_POSTFIELDS => $queryData,
+    ));
 
-        $queryData = http_build_query(array_merge($params, array("auth" => $_REQUEST['AUTH_ID'])));
-            
-        
-
-        $curl = curl_init();
-        curl_setopt_array($curl, array(
-            CURLOPT_SSL_VERIFYPEER => 0,
-            CURLOPT_POST => 1,
-            CURLOPT_HEADER => 0,
-            CURLOPT_RETURNTRANSFER => 1,
-            CURLOPT_URL => $queryUrl,
-            CURLOPT_POSTFIELDS => $queryData,
-        ));
-
-        $result = json_decode(curl_exec($curl), true);
-        curl_close($curl);
-       
-        out($result);
-
-        function out($var, $var_name = ''){
-            echo '<pre style="outline: 1px dashed red; padding: 5px;margin: 10px;color: white; background: black;">';
-            if (is_string($var)){
-                $var = htmlspecialchars($var);
-
-            }
-            print_r($var);
-            echo '</pre>';
-        }
+    $result = curl_exec($curl);
+    curl_close($curl);
 
 
-
-
-?>
-
-<table style="border-collapse: collapse;" border="1">
-    <?php foreach ($result['result'] as $user) {?>
-    <tr>
-        <td><?=$user['EMAIL']?></td>
-        <td><?=$user['NAME']?></td>
-        <td><?=$user['LAST_NAME'] ?></td>
-        
-    </tr>
-    <?php } ?>
-</table>
-
-
-</div>
-</body>
-</html>
-
-    
+    echo "<pre>";
+    print_r($result);
+    print_r(json_decode($result, true));
+    echo "</pre>";
